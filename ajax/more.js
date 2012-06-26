e32a563321f9936f0bc5bd1a532a888838c359c4
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2012-06-27 01:19:37 +0300

    Initial start on ajax example

---
+++ ajax/more.js
@@ -0,0 +1,4 @@
+Kas näin! Et kyllä olisi arvannut, mitä tähän tulee. Tämä teksti siis lukee ihan toisesta tiedostosta. <br/>
+
+Miksi se sitten on <em>niin</em> hienoa? Koska tätä kautta voisi ladata tietoa joistain toisesta palvelusta.
+Esimerkiksi sinun verkkosivusi voisi hyödyntää Helsingin seudun reittiopasta ns. rajapinnan (application programming interface, API) kautta. <br/>
